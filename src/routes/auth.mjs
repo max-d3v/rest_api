92d0df44c6afb3797f7b62cb53e436d41bdb912f
max <maxbuzzarello@gmail.com>
2024-04-15 08:33:54 -0300

    improved auth endpoint so it becomes more intuitive

--- src/routes/auth.mjs
+++ src/routes/auth.mjs
@@ -6,13 +6,16 @@ const router = Router();
 router.post('/auth', (req, res, next) => {
     passport.authenticate('local', function(err, user, info) {
         if (err) { 
-            return res.sendStatus(500); 
+            return next(err); 
+        }
+        if (!user) { 
+            return res.status(401).json(info); 
         }
         req.logIn(user, function(err) {
             if (err) { 
                 return next(err); 
             }
-            return res.status(200).send({status: "success", msg: "User authenticated", user: user});
+            return res.status(200).json({ sessionID: req.sessionID, cookie: req.session.cookie});
         });
     })(req, res, next); // Adicione isto
 });
