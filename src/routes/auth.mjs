9e409ef2005ebb4a8d45b83012fef80d3feb1807
max <maxbuzzarello@gmail.com>
2024-05-05 21:08:27 -0300

    rota de auth

--- src/routes/auth.mjs
+++ src/routes/auth.mjs
@@ -3,22 +3,22 @@ import passport from "passport";
 const router = Router();
 
 //Auth login route
-router.post('/auth', (req, res, next) => {
+router.post('/auth/login', (req, res, next) => {
     passport.authenticate('local', function(err, user, info) {
         if (err) { 
             return next(err); 
         }
         if (!user) { 
-            return res.status(401).json(info); 
+            return res.status(200).send({status: "error", message: info}); 
         }
         req.logIn(user, function(err) {
             if (err) { 
                 return next(err); 
             }
-            return res.status(200).json({ sessionID: req.sessionID, cookie: req.session.cookie});
+            return res.status(200).send({ status: "success", data: req.sessionID });
         });
     })(req, res, next); // Adicione isto
-});
+});     
 
 
 // ----------------------------
