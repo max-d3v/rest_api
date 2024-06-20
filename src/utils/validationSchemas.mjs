89371a4ffbec1d17c0d3e5205fe3db7764f5f994
max-d3v <max.maul.desenvolvedor@gmail.com>
2024-06-20 15:24:54 -0300

    all endpoints validation schemas

--- src/utils/validationSchemas.mjs
+++ src/utils/validationSchemas.mjs
@@ -10,3 +10,68 @@ export const exampleRouteValidation = {
     }
 } 
 // ----------------------------
+export const CreateBicicletaValidation = {
+    quadro_bicicleta: {
+        isFloat: {
+            errorMessage: "Quadro must be a number",
+        },
+        notEmpty: {
+            errorMessage: "Quadro cannot be empty",
+        }
+    },
+    cor_bicicleta: {
+        isString: {
+            errorMessage: "Cor must be a string",
+        },
+        notEmpty: {
+            errorMessage: "Cor cannot be empty",
+        }
+    },
+    
+}
+
+export const CreateEscolhaValidation = {
+    codigo_interessado: {
+        isInt: {
+            errorMessage: "Codigo escolhido must be a number",
+        },
+        notEmpty: {
+            errorMessage: "Codigo escolhido cannot be empty",
+        }
+    }
+}
+
+export const CreateInteressadoValidation = {
+    nome_interessado: {
+        isString: {
+            errorMessage: "Nome must be a string",
+        },
+        notEmpty: {
+            errorMessage: "Nome cannot be empty",
+        }
+    },
+    email_interessado: {
+        isEmail: {
+            errorMessage: "Email must be a valid email",
+        },
+        notEmpty: {
+            errorMessage: "Email cannot be empty",
+        }
+    },
+    fone_interessado: {
+        isString: {
+            errorMessage: "Fone must be a string",
+        },
+        notEmpty: {
+            errorMessage: "Fone cannot be empty",
+        }
+    },
+    altura_interessado_cm: {
+        isInt: {
+            errorMessage: "Altura must be a number",
+        },
+        notEmpty: {
+            errorMessage: "Altura cannot be empty",
+        }
+    }
+}
