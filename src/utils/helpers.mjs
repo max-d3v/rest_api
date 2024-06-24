c8468872fc806baff1ffd6e793521b93b217e4fb
max-d3v <max.maul.desenvolvedor@gmail.com>
2024-06-24 14:22:35 -0300

    put function to find bicicle quadro in helper

--- src/utils/helpers.mjs
+++ src/utils/helpers.mjs
@@ -7,3 +7,19 @@ export const hashSenha = (senha) => {
     const salt = bcrypt.genSaltSync(saltRounds);
     return bcrypt.hashSync(senha, salt)
 }
+
+export const FindQuadroBicicleta = (altura_interessado_cm) => {    
+    var selectedQuadro;
+    if (altura_interessado_cm >= 150 && altura_interessado_cm <= 160 ) {
+        selectedQuadro = 14;
+    }
+    else if (altura_interessado_cm > 160 && altura_interessado_cm <= 170 ) {
+        selectedQuadro = 16;
+    }
+    else if ( altura_interessado_cm > 170 ) {
+        selectedQuadro = 17;
+    } else {
+        return null;
+    }
+    return selectedQuadro
+}
