8e099a3b8aed8c8dae8d664f56f4fb68a608a6d5
max-d3v <max.maul.desenvolvedor@gmail.com>
2024-06-24 14:22:09 -0300

    delete endpoint and fixing refactoring patch endpoint

--- src/routes/escolha.mjs
+++ src/routes/escolha.mjs
@@ -2,7 +2,8 @@ import {Router} from "express";
 import { checkSchema } from "express-validator";
 import { matchedData,validationResult } from "express-validator";
 import { PrismaClient } from "@prisma/client";
-import { CreateEscolhaValidation } from "../utils/validationSchemas.mjs";
+import { CreateEscolhaValidation, alterEscolhaValidation } from "../utils/validationSchemas.mjs";
+import { FindQuadroBicicleta } from "../utils/helpers.mjs";
 
 const prisma = new PrismaClient();
 const router = Router();
@@ -65,48 +66,141 @@ router.post('/', checkSchema(CreateEscolhaValidation),  async (req, res) => {
         return res.status(404).send({"error": "Interessado not found"});
     }
 
-    var selectedQuadro = null;
     const { altura_interessado_cm } = interessado;
     if (!altura_interessado_cm) {
         return res.status(400).send({"error": "Altura do interessado não informada"});
     }
-    
-    if (altura_interessado_cm >= 150 && altura_interessado_cm <= 160 ) {
-        selectedQuadro = 14;
-    }
-    else if (altura_interessado_cm > 160 && altura_interessado_cm <= 170 ) {
-        selectedQuadro = 16;
-    }
-    else if ( altura_interessado_cm > 170 ) {
-        selectedQuadro = 17;
-    }
+    const selectedQuadro = FindQuadroBicicleta(altura_interessado_cm)
 
     if (!selectedQuadro) {
         return res.status(400).send({"error": "Erro ao calcular altura do quadro"});
     }
 
-    const selectedBicicleta = await prisma.bicicleta.findFirst({
+    const bicicletas = await prisma.bicicleta.findMany({
         where: {
             quadro_bicicleta: selectedQuadro
         }
     })
-    if (!selectedBicicleta) {
+    if (bicicletas.length == 0) {
         return res.status(404).send({"error": "Bicicleta com aro desejado não encontrada"});
     }
 
-    const {codigo_bicicleta} = selectedBicicleta;
-
+    var codigos = [];
+    bicicletas.forEach(bicicleta => {
+        codigos.push(bicicleta.codigo_bicicleta);
+    });
     
-        var escolha = await prisma.escolha.create({
-            data: {
-                codigo_bicicleta,
-                codigo_interessado,
+    var escolha = await prisma.escolha.create({
+        data: {
+            codigos_bicicletas: codigos,
+            codigo_interessado,
+        }
+    })
+    res.status(201).send({"data": escolha});
+    }
+    catch(err) {
+        console.log(err)
+        res.status(500).send({"error": err});
+    }
+});
+
+
+router.patch("/:id", checkSchema(alterEscolhaValidation), async (req, res) => {
+    const {id} = req.params;
+    if (!id || isNaN(id)) {
+        res.status(400).send({"error": "Invalid ID"});
+        return;
+    }
+    const result = validationResult(req);
+    if (!result.isEmpty()) {
+        console.log("passou aqui");
+        return res.status(400).send({"error": result.array()});
+    }
+    const data = matchedData(req);
+    const { codigo_interessado } = data;
+    if (!codigo_interessado) {
+        res.status(400).send({"error": "Invalid data"});
+        return;
+    }
+    try {
+        const escolha = await prisma.escolha.findUnique({
+            where: {
+                codigo_escolha: parseInt(id)
             }
-        })
-        escolha.quadro_bicicleta = selectedQuadro;
+        });
+        if (!escolha) {
+            res.status(404).send({"error": "Escolha not found"});
+            return;
+        }
+
+        const interessado = await prisma.interessado.findUnique({
+            where: {
+                codigo_interessado: escolha.codigo_interessado
+            }
+        });
+        if (!interessado) {
+            res.status(404).send({"error": "Interessado not found"});
+            return;
+        }
+        const { altura_interessado_cm } = interessado;
+        var selectedQuadro = FindQuadroBicicleta(altura_interessado_cm);
+
+        const bicicletas = await prisma.bicicleta.findMany({
+            where: {
+                quadro_bicicleta: selectedQuadro
+            }
+        });
+        if (bicicletas.length == 0) {
+            res.status(404).send({"error": "Bicicleta not found"});
+            return;
+        }
+        var codigos = [];
+        bicicletas.forEach(bicicleta => {
+            codigos.push(bicicleta.codigo_bicicleta);
+        });
 
 
-        res.status(201).send({"data": escolha});
+        const escolhaUpdated = await prisma.escolha.update({
+            where: {
+                codigo_escolha: parseInt(id)
+            },
+            data: {
+                codigo_interessado,
+                codigos_bicicletas: codigos
+            }
+        });
+
+        res.status(200).send({"data": escolhaUpdated});
+    }
+    catch(err) {
+        console.log(err)
+        res.status(500).send({"error": err});
+    }
+});
+
+
+router.delete("/:id", async (req, res) => {
+    const {id} = req.params;
+    if (!id || isNaN(id)) {
+        res.status(400).send({"error": "Invalid ID"});
+        return;
+    }
+    try {
+        const escolha = await prisma.escolha.findUnique({
+            where: {
+                codigo_escolha: parseInt(id)
+            }
+        });
+        if (!escolha) {
+            res.status(404).send({"error": "Escolha not found"});
+            return;
+        }
+        await prisma.escolha.delete({
+            where: {
+                codigo_escolha: parseInt(id)
+            }
+        });
+        res.status(204).send();
     }
     catch(err) {
         res.status(500).send({"error": err});
